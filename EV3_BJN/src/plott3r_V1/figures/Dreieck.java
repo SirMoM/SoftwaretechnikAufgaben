3878e6cb85b49ad64b8a9975d6396662af2db0fc
Noah Ruben <noah.ruben@online.de>
2018-11-20 12:02:12 +0100

    add dreieck

---
+++ EV3_BJN/src/plott3r_V1/figures/Dreieck.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package plott3r_V1.figures;
+
+import java.util.List;
+
+import plott3r_V1.Instruction;
+
+/**
+ * @author Noah Ruben
+ * @see <a href="https://github.com/SirMoM/SoftwaretechnikAufgaben">Github</a>
+ */
+public class Dreieck extends GeoFig{
+
+	/* (non-Javadoc)
+	 * @see plott3r_V1.figures.GeoFig#getInstructionSet()
+	 */
+	@Override
+	public List<Instruction> getInstructionSet(){
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
